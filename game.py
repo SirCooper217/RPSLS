772898802972a04c2f994ad9fd79c243e6b3b439
SirCooper217 <ryanjc1597@yahoo.com>
2022-12-05 16:25:28 -0600

    Modified Participant and Game

---
+++ game.py
@@ -0,0 +1,20 @@
+from user import User
+from com import Computer
+from time import sleep
+
+class Game:
+    def __init__(self):
+        pass
+    
+    def run_game(self, participant):
+        player_one = User("User")
+        print("\n Welcome to RPSLS! \nEach game is a best of three! The rules are:")
+        sleep(1)
+        print("\nRock crushes Scissors \nScissors cuts Paper \nPaper covers Rock \nRock crushes Lizard \nLizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock \nSpock vaporizes Rock")
+        sleep(2.5)
+        input("Would you like to play against the AI, or against another player?")
+        
+        pass
+
+    def display_winner(self):
+        pass
--- participant.py
+++ participant.py
@@ -1,4 +1,4 @@
 class Participant:
     def  __init__(self, name):
         self.name = name
-        
+        self.score = 0
